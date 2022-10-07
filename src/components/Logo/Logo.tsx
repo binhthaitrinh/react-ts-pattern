01216c79086a85d1129aac61c24c10739f7ca557
Kyle Trinh <kyle.trinh94@gmail.com>
2022-10-07 16:21:55 -0700

    Initial commit

---
+++ src/components/Logo/Logo.tsx
@@ -0,0 +1,17 @@
+import styles from './Logo.module.css';
+
+export const Logo = () => {
+  return (
+    <h1 className={styles.logo}>
+      <span role='img' aria-label='metal hand emoji'>
+        🤟
+      </span>
+      <span role='img' aria-label='musical ketyboard emoji'>
+        🎹
+      </span>
+      <span role='img' aria-label='musical notes emoji'>
+        🎶
+      </span>
+    </h1>
+  );
+};
